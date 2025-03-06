8124439a2f5ab52c0b5ed58ac3f2f6542ee1c2f0
Yaman Mahtha <yamanmahtha@gmail.com>
2025-03-06 15:58:26 +0530

    [YAMAN] add. Calculating the daily wages of employee

--- EmployeeWages/EmployeeMain.js
+++ EmployeeWages/EmployeeMain.js
@@ -1,3 +1,4 @@
+//UC1-EmployeePresence
 const EMPLOYEE_PRESENT = 1;
 
 let empCheck = Math.floor(Math.random() * 2);
@@ -6,3 +7,27 @@ if (empCheck === EMPLOYEE_PRESENT) {
 } else {
     console.log("Employee is Absent");
 }
+//UC-2 Calculate Daily Employee Wage
+const WAGE_PER_HOUR = 10;
+const FULL_TIME_HOURS = 5;
+const PART_TIME_HOURS = 1;
+// Generate random work type (0 - No Work, 1 - Part Time, 2 - Full Time)
+let empType = Math.floor(Math.random() * 3);
+let workHours;
+switch (empType) {
+    case 1:
+        workHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time.");
+        break;
+    case 2:
+        workHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time.");
+        break;
+    default:
+        workHours = 0;
+        console.log("Employee did not work.");
+        break;
+}
+// Calculate daily wage
+let dailyWage = workHours * WAGE_PER_HOUR;
+console.log(`Daily Wage: $${dailyWage}`);
